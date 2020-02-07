2482e70fadfb9b94ccbd0f616e76617482c47356
Emanuele Ballarin <emanuele@ballarin.cc>
2020-02-07 04:25:44 +0100

    Reformat, add some members, fix .gitignore

--- include/bst.hpp
+++ include/bst.hpp
@@ -1,59 +1,59 @@
-#include <utility>
 #include <node.hpp>
+#include <utility>
 
 
-template<typename Key, typename Value, typename Cmp=std::less<Key>(default)>
-class bst{
-public:
-	using key_type = Key;
-	using value_type = Value;
-	using node_type = Node<std::pair<key_type, value_type>>;
-	using iterator = iterator<node_type>
-	//using const_iterator
-	
-	bst()=default;
-	bst
+template<typename Key, typename Value, typename Cmp = std::less<Key>(default)>
+class bst
+{
+    public:
+    using key_type = Key;
+    using value_type = Value;
+    using node_type = Node<std::pair<key_type, value_type>>;
+    using iterator = iterator<node_type>;
+    //using const_iterator
 
-	//insert
-	std::pair<iterator, bool> insert(const pair_type& x);
-	std::pair<iterator, bool> insert(pair_type&& x);	
+    bst() = default;
+    bst
 
-	//emplace
-	template< class... Types >
-	std::pair<iterator,bool> emplace(Types&&... args);
+      //insert
+      std::pair<iterator, bool>
+      insert(const pair_type& x);
+    std::pair<iterator, bool> insert(pair_type&& x);
 
-	//clear
-	void clear();
-	
-	//begin
-	iterator begin();
-	const_iterator begin() const;
-	const_iterator cbegin() const;
+    //emplace
+    template<class... Types>
+    std::pair<iterator, bool> emplace(Types&&... args);
 
-	//end
-	iterator end();
-	const_iterator end() const;
-	const_iterator cend() const;
+    //clear
+    void clear();
 
-	//find
-	iterator find(const key_type& x);
-	const_iterator find(const key_type& x) const;
+    //begin
+    iterator begin();
+    const_iterator begin() const;
+    const_iterator cbegin() const;
 
-	//balance
-	void balance();
-	
-	//subscripting
-	value_type& operator[](const key_type& x);
-	value_type& operator[](key_type&& x);
+    //end
+    iterator end();
+    const_iterator end() const;
+    const_iterator cend() const;
 
-	//Put-to operator
-	friend std::ostream& operator<<(std::ostream& os, const bst& x);
+    //find
+    iterator find(const key_type& x);
+    const_iterator find(const key_type& x) const;
 
-	//erase
-	void erase(const key_type& x);
+    //balance
+    void balance();
 
-private:
-	Node* root;
-}
+    //subscripting
+    value_type& operator[](const key_type& x);
+    value_type& operator[](key_type&& x);
 
+    //Put-to operator
+    friend std::ostream& operator<<(std::ostream& os, const bst& x);
 
+    //erase
+    void erase(const key_type& x);
+
+    private:
+    Node* root;
+};
--- include/node.hpp
+++ include/node.hpp
@@ -1,33 +1,85 @@
+#include <cassert>  // Run-time asserts
 #include <memory>
 #include <utility>
 
 template<typename T>
-class Node{
-public:
-	using value_type = T;
-	
-	Node()=default;//?change default values
-	Node(std::unique_ptr<Node> lc, std::unique_ptr<Node> rc, std::shared_ptr<Node> pa);//implement asserts
-	
-	Node& Node(const Node&);
-	const Node& Node(const Node&) const;
+class Node
+{
+    public:
+    using value_type = T;
 
-	Node& Node(Node&& node);//?move unique_ptr
-	const Node& Node(const Node&& node);//?move unique_ptr
+    Node() = default;
+    Node(std::unique_ptr<Node> lc, std::unique_ptr<Node> rc, std::shared_ptr<Node> pa);  //implement asserts
 
-	~Node();
+    // Constructors have always 'the class they construct' as return type... | Copyee must be const-qualified
+    Node(const Node&) = delete;
 
-	Node& operator=(const Node&);
-	const Node& operator=(const Node&);
+    // Constructors have always 'the class they construct' as return type...
+    Node(Node&& node);
+    Node(const Node&& node);  // Only useful if we want to build nodes from FUNCTIONS that return 'const Node&&'
+                              // Maybe remove?
 
-	Node& operator=(Node&& node);
-	const Node& operator=(const Node&& node);
-	
-private:
-	T elem;
-	std::unique_ptr<Node> left_child;
-	std::unique_ptr<Node> right_child;
-	std::shared_ptr<Node> parent;//?default initialization shared_pointer
+    ~Node() = default;  // ENOUGH??
 
+    // If the LHS is const, we can just construct; never assign.
+    Node& operator=(const Node&) = delete;
+
+    // We have 4 to choose from only 2 (cannot overload on ret-type alone)
+    // If we const-qualify LHS, what does it mean to assign to?
+    Node& operator=(Node&& node)
+    {
+        elem = std::move(node.elem);
+        left_child = std::move(node.left_child);
+        right_child = std::move(node.right_child);
+        parent = std::move(node.parent);
+        return *this;
+    };
+    //const Node& operator=(Node&& node);
+    Node& operator=(const Node&& node)  // Exact copy of the above...
+    {
+        elem = std::move(node.elem);
+        left_child = std::move(node.left_child);
+        right_child = std::move(node.right_child);
+        parent = std::move(node.parent);
+        return *this;
+    };  // Only useful if we want to build nodes from FUNCTIONS that return 'const Node&&'
+        // Maybe remove?
+    //const Node& operator=(const Node&& node);
+
+    //    const T get_elem()  // What to return?
+    //    {
+    //        return elem;
+    //    }
+
+    private:
+    T elem;  // Beware of the case in which T is a native type |-> garbage values
+    std::unique_ptr<Node> left_child;
+    std::unique_ptr<Node> right_child;
+    std::shared_ptr<Node> parent;
+};
+
+template<typename T>
+Node<T>::Node(std::unique_ptr<Node> lc, std::unique_ptr<Node> rc, std::shared_ptr<Node> pa) :
+    left_child{lc}, right_child{rc}, parent{pa}
+{
+    assert(((lc != rc) || (lc == nullptr && rc == nullptr))
+           && "Left and Right children cannot be the same, unless null pointers.");
+    assert(((lc != pa && lc != pa) || (lc == nullptr && rc == nullptr && pa == nullptr))
+           && "Left and/or Right children cannot be the same as the parent, unless all are null pointers.");
 }
 
+
+template<typename T>
+Node<T>::Node(Node&& node) :
+    elem{std::move(node.elem)}, left_child{std::move(node.left_child)},
+    right_child{std::move(node.right_child)}, parent{std::move(node.parent)}
+{
+}
+
+// An exact copy of that above...
+template<typename T>
+Node<T>::Node(const Node&& node) :
+    elem{std::move(node.elem)}, left_child{std::move(node.left_child)},
+    right_child{std::move(node.right_child)}, parent{std::move(node.parent)}
+{
+}
