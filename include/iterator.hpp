e89981edf2bdeed684c377a49b479917be272f88
Leonardo Arrighi <Leonardoarrighi43@gmail.com>
2020-02-09 20:04:49 +0100

    "iterator"

--- include/iterator.hpp
+++ include/iterator.hpp
@@ -1,7 +1,8 @@
-#include <memory>
+#include <iostream>
 #include <iterator>
 #include "bst.hpp"
-#include <iostream>
+#include "node.hpp"
+#include <memory>
 
 template<typename node>
 class iterator{
@@ -11,6 +12,7 @@ class iterator{
         using pointer = value_type*;
         using iterator_category = std::forward_iterator_tag;
 
+
         iterator& operator++();
         iterator operator++(int);
         reference operator*() const;
@@ -18,10 +20,40 @@ class iterator{
         bool operator==(const iterator& rhs) const;
         bool operator!=(const iterator& rhs) const;
 
-
-
     private:
         Node* current;
-
-    
 };
+
+template<typename node>
+iterator<node>& iterator<node>::operator++(){
+    //++current;
+    //return * this;
+}
+
+template<typename node>
+iterator<node> iterator<node>::operator++(int){
+    //iterator tmp(*this);
+    //operator++();
+    //return tmp;
+}
+
+template<typename node>
+iterator<node>::reference iterator<node>::operator*() const{
+    return * current;
+
+}
+
+template<typename node>
+iterator<node>::pointer iterator<node>::operator->() const{
+    return current;
+}
+
+template<typename node>
+bool iterator<node>::operator==(const iterator& rhs) const{
+    return current==rhs.current;
+}
+
+template<typename node>
+bool iterator<node>::operator!=(const iterator& rhs) const{
+    return current!=rhs.current;
+}
