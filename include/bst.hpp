ea3e447caf334e258310703e0395acf54dd57f8a
Emanuele Ballarin <emanuele@ballarin.cc>
2020-02-12 01:27:35 +0100

    Local changes to bst.hpp

--- include/bst.hpp
+++ include/bst.hpp
@@ -3,6 +3,7 @@
 #include "node.hpp"
 
 #include <iostream>
+#include <queue>
 #include <utility>
 #include <vector>
 
@@ -17,7 +18,21 @@ class bst
     using iterator = tree_iterator<node_type, false>;
     using const_iterator = tree_iterator<node_type, true>;
 
+    // default ctor
     bst() = default;
+
+    // cpctor
+    bst(const bst&);
+
+    // mvctor
+    bst(bst&&) noexcept = default;
+
+    // cpasst
+    bst& operator=(bst&);
+
+    //mvasst
+    bst& operator=(bst&&) noexcept = default;
+
 
     //insert
     std::pair<iterator, bool> insert(const pair_type& x);
@@ -52,19 +67,85 @@ class bst
     value_type& operator[](key_type&& x);
 
     //Put-to operator
-    friend std::ostream& operator<<(std::ostream& os, const bst& x);
+    friend std::ostream& operator<<(std::ostream& os, const bst<kt, vt, cmp>& x)
+    {
+        // Do stuff here!
+        return os;
+    };
 
     //erase
     void erase(const key_type& x);
 
     private:
-    Node<pair_type>* root;
+    std::unique_ptr<node_type> root;
+    //node_type* root;
     std::vector<node_type*> ordered_vec;
 
     //wrapper for find: bool=true returns end, bool=false returns where to insert
     iterator find_private(const key_type&, bool);
     const_iterator find_private(const key_type&, bool) const;
 };
+
+
+// cpctor
+template<typename kt, typename vt, typename cmp>
+bst<kt, vt, cmp>::bst(const bst<kt, vt, cmp>& original)
+{
+    if (!(original->root))
+    {
+        this->root = nullptr;
+    }
+    else
+    {
+        std::queue<node_type*> queue_original{};
+        std::queue<node_type*> queue_this{};
+
+        queue_original.push(original->root);
+        queue_this.push(this->root);
+
+        bool has_children;
+
+        while (!queue_original.empty())
+        {
+            has_children = (queue_original.front()->read_lc()->get()->read_lc()->get()
+                            || queue_original.front()->read_rc()->get()->read_rc()->get());
+            // Sx
+            if (queue_original.front()->read_lc()->get())
+            {
+                queue_this.front()->set_lc(new node_type(queue_original.front()->read_lc()->read_elem()));
+                if (has_children)
+                {
+                    queue_original.push(queue_original.front()->read_lc()->get());
+                    queue_this.push(queue_this.front()->read_lc()->get());
+                }
+            }
+
+            // Dx
+            if (queue_original.front()->read_rc()->get())
+            {
+                queue_this.front()->set_rc(new node_type(queue_original.front()->read_rc()->read_elem()));
+                if (has_children)
+                {
+                    queue_original.push(queue_original.front()->read_rc()->get());
+                    queue_this.push(queue_this.front()->read_rc()->get());
+                }
+            }
+
+            queue_original.pop();
+            queue_this.pop();
+        }
+    }
+};
+
+// cpasst
+template<typename kt, typename vt, typename cmp>
+bst<kt, vt, cmp>& bst<kt, vt, cmp>::operator=(bst<kt, vt, cmp>& original)
+{
+    bst<kt, vt, cmp> copy{original};
+    this = &copy;
+    return *this;
+};
+
 
 /*They are used to insert a new node. The function returns a pair of an iterator
 (pointing to the node) and a bool. The bool is true if a new node has been
