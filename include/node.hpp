7ef299596bcd2058031fc7b4205e2bacf161eaf9
Emanuele Ballarin <emanuele@ballarin.cc>
2020-02-13 22:02:10 +0100

    Revert "[DIAG] Instrument node.hpp"
    This reverts commit ff420363355583d1e891a18bfd0108e3a8e3f0e2.

--- include/node.hpp
+++ include/node.hpp
@@ -99,26 +99,12 @@ inline Node<T>::Node(FWR&& given) noexcept : elem{std::forward<FWR>(given)}
 template<typename T>
 inline Node<T>::Node(Node&& node) noexcept :
     elem{std::move(node.elem)}, left_child{std::move(node.left_child)},
-    right_child{std::move(node.right_child)}, parent{std::move(node.parent)}
-{
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Move ctor"
-              << "\n"
-              << std::endl;
-#endif
-};
+    right_child{std::move(node.right_child)}, parent{std::move(node.parent)} {};
 
 
 template<typename T>
 inline Node<T>& Node<T>::operator=(Node<T>&& node) noexcept
 {
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Mvasst!"
-              << "\n"
-              << std::endl;
-#endif
     this->elem = std::move(node.elem);
     this->left_child = std::move(node.left_child);
     this->right_child = std::move(node.right_child);
@@ -130,48 +116,24 @@ inline Node<T>& Node<T>::operator=(Node<T>&& node) noexcept
 template<typename T>
 inline const T& Node<T>::read_elem() const noexcept
 {
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Read Element!"
-              << "\n"
-              << std::endl;
-#endif
     return this->elem;
 };
 
 template<typename T>
 inline const std::unique_ptr<Node<T>>& Node<T>::read_lc() const noexcept
 {
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Read LC (uniqueptr)!"
-              << "\n"
-              << std::endl;
-#endif
     return this->left_child;
 };
 
 template<typename T>
 inline const std::unique_ptr<Node<T>>& Node<T>::read_rc() const noexcept
 {
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Read RC (uniqueptr)!"
-              << "\n"
-              << std::endl;
-#endif
     return this->right_child;
 };
 
 template<typename T>
 inline const std::experimental::observer_ptr<Node<T>>& Node<T>::read_pr() const noexcept
 {
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Read PR (Observer ptr)!"
-              << "\n"
-              << std::endl;
-#endif
     return this->parent;
 };
 
@@ -179,12 +141,6 @@ template<typename T>
 template<typename FWR>
 inline void Node<T>::write_elem(FWR&& given)
 {
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: FWDing write Element!"
-              << "\n"
-              << std::endl;
-#endif
     this->elem = std::forward<FWR>(given);
 };
 
@@ -195,13 +151,6 @@ inline void Node<T>::set_lc(Node<T>* given)
     assert(given);                      // Can't set to nullptr this way; use null_left() instead
     assert(!(this->left_child.get()));  // Can set only if free
     assert(!(this->parent.get()) || this->parent.get() != given);  // Can't create cycles
-
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Set LC (from NAKED PTR)!"
-              << "\n"
-              << std::endl;
-#endif
 
     this->left_child.reset(given);
     this->left_child.get()->set_parent(this);
@@ -215,13 +164,6 @@ inline void Node<T>::set_rc(Node<T>* given)
     assert(!(this->right_child.get()));  // Can set only if free
     assert(!(this->parent.get()) || this->parent.get() != given);  // Can't create cycles
 
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Set RC (from NAKED PTR)!"
-              << "\n"
-              << std::endl;
-#endif
-
     this->right_child.reset(given);
     this->right_child.get()->set_parent(this);
     this->right_child.get()->set_childtype(true);
@@ -230,13 +172,6 @@ inline void Node<T>::set_rc(Node<T>* given)
 template<typename T>
 inline void Node<T>::set_children(Node<T>* l_given, Node<T>* r_given) noexcept
 {
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Set BOTH CHILDREN (double call)!"
-              << "\n"
-              << std::endl;
-#endif
-
     this->set_lc(l_given);
     this->set_rc(r_given);
 }
@@ -244,13 +179,6 @@ inline void Node<T>::set_children(Node<T>* l_given, Node<T>* r_given) noexcept
 template<typename T>
 inline void Node<T>::detach_children() noexcept
 {
-
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Detach BOTH CHILDREN!"
-              << "\n"
-              << std::endl;
-#endif
     this->detach_left();
     this->detach_right();
 }
@@ -258,12 +186,6 @@ inline void Node<T>::detach_children() noexcept
 template<typename T>
 inline void Node<T>::null_children() noexcept
 {
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Null BOTH CHILDREN!"
-              << "\n"
-              << std::endl;
-#endif
     this->null_left();
     this->null_right();
 };
@@ -272,14 +194,6 @@ inline void Node<T>::null_children() noexcept
 template<typename T>
 inline void Node<T>::detach_left() noexcept
 {
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Detach LC!"
-              << "\n"
-              << std::endl;
-#endif
-
-
     if (this->left_child)
     {
         this->left_child.get()->null_parent();
@@ -291,13 +205,6 @@ inline void Node<T>::detach_left() noexcept
 template<typename T>
 inline void Node<T>::detach_right() noexcept
 {
-
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Detach RC!"
-              << "\n"
-              << std::endl;
-#endif
     if (this->right_child)
     {
         this->right_child.get()->null_parent();
@@ -309,12 +216,6 @@ inline void Node<T>::detach_right() noexcept
 template<typename T>
 inline void Node<T>::null_left() noexcept
 {
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Null LC!"
-              << "\n"
-              << std::endl;
-#endif
     if (this->left_child)
     {
         this->left_child.reset();
@@ -324,12 +225,6 @@ inline void Node<T>::null_left() noexcept
 template<typename T>
 inline void Node<T>::null_right() noexcept
 {
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Null RC!"
-              << "\n"
-              << std::endl;
-#endif
     if (this->right_child)
     {
         this->right_child.reset();
@@ -339,61 +234,30 @@ inline void Node<T>::null_right() noexcept
 template<typename T>
 inline void Node<T>::null_parent() noexcept
 {
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Null parent!"
-              << "\n"
-              << std::endl;
-#endif
     this->parent.reset();
 }
 
 template<typename T>
 inline void Node<T>::set_parent(Node<T>* newparent) noexcept
 {
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Set parent!"
-              << "\n"
-              << std::endl;
-#endif
-
     this->parent.reset(newparent);
 }
 
 template<typename T>
 inline void Node<T>::set_childtype(bool childbool) noexcept
 {
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: Set Childtype"
-              << "\n"
-              << std::endl;
-#endif
     this->childtype = childbool;
 }
 
 template<typename T>
 inline bool Node<T>::is_left() noexcept
 {
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called: IsLeft?!"
-              << "\n"
-              << std::endl;
-#endif
     return !this->childtype;
 }
 
 template<typename T>
 inline bool Node<T>::is_right() noexcept
 {
-#if defined(DIAG)
-    std::cout << "[DIAG]: "
-              << "Called:IsRight?!"
-              << "\n"
-              << std::endl;
-#endif
     return this->childtype;
 }
 
