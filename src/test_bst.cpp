cdaf197d05b82bcb070e05b6f1248d1c88996464
Emanuele Ballarin <emanuele@ballarin.cc>
2020-02-14 15:36:50 +0100

    Update test

--- src/test_bst.cpp
+++ src/test_bst.cpp
@@ -4,8 +4,15 @@
 
 int main()
 {
-    int unused;
+    auto tree = new bst<int, int>();
 
-    std::cout << "Hello, world!" << std::endl;
+    tree->insert(std::pair<int, int>({1, 10}));
+    tree->insert(std::pair<int, int>({2, 20}));
+    tree->insert(std::pair<int, int>({3, 30}));
+    //tree->insert(std::pair<int, int>({4, 40}));
+    //tree->insert(std::pair<int, int>({5, 50}));
+    //tree->insert(std::pair<int, int>({6, 60}));
+
+    delete tree;
     return 0;
 }
