5cca022a7c6e729c3e9da8ff35774cfcc8a73f9d
fromor <francesco.romor@gmail.com>
2020-02-18 17:18:35 +0100

    comments on iterator

--- include/iterator.hpp
+++ include/iterator.hpp
@@ -19,18 +19,12 @@
 #pragma once  // Easy ODR
 
 #include "node.hpp"
-//#include "bst.hpp"
 
 #include <iostream>
 #include <iterator>
 #include <memory>
 #include <type_traits>
 
-// struct Spam {
-//   static Node<int> eggs;
-// };
-//
-// static Node<int> eggs{};
 /**
  * @brief Type-templated forward tree_iterator
  * @tparam node Typename of the node
@@ -50,27 +44,56 @@ class tree_iterator
 
     friend tree_iterator<node, !Const>;
 
-    constexpr inline tree_iterator() noexcept = default;
+    tree_iterator() noexcept = delete; //deleted since it would coincide with end
+    /**
+     * @brief custom constructor
+     * @param given The pointer to the node
+     */
     explicit tree_iterator(value_type* given) noexcept : current{given} {}
 
+    /**
+     * @brief constructor from const or non const iterator
+     * @param given The iterator to be copied and eventually const casted
+     */
     template<bool T>
     tree_iterator(tree_iterator<node, T> given) noexcept
     {
         this->current = const_cast<pointer>(given.current);
     }
 
+    /**
+     * @brief Dereference operator
+     * @return reference to the current node
+     */
     reference operator*() const{ return *current;}
+
+    /**
+     * @brief Access member of current
+     * @return pointer to current node
+     */
     pointer operator->() const{return &(*(*this));}
 
     tree_iterator operator++() noexcept;
     inline tree_iterator operator++(int) noexcept;  // Acceptable warning (clang-tidy)
 
+    /**
+     * @brief Equality operator
+     * @tparam T Bool to compare also const interators with non-const iterators
+     * @param given The iterator to be compared to
+     * @return bool
+     */
     template<bool T>
     bool operator==(const tree_iterator<node, T>& given) const noexcept
     {
       return this->current == given.current;
     }
 
+    /**
+     * @brief Inequality operator
+     * @tparam T Bool to compare also const interators with non-const iterators
+     * @param given The iterator to be compared to
+     * @return bool
+     */
     template<bool T>
     bool operator!=(const tree_iterator<node, T>& given) const noexcept
     {
@@ -80,15 +103,18 @@ class tree_iterator
     inline tree_iterator next() noexcept;
 
     private:
+    /** Current node to be pointed to by the iterator*/
     value_type* current;
 
     inline tree_iterator<node, Const> leftmost(tree_iterator<node, Const>&) noexcept;
 };
 
 /**
- * @brief Type-templated forward tree_iterator
+ * @brief Pre-increment of forward iterator
  * @tparam node Typename of the node
  * @tparam Const Bool to represent a const iterator (true) or an iterator (false)
+ *
+ * Traverse the tree in order to find the next node to point to.
  */
 template<typename node, bool Const>
 tree_iterator<node, Const> tree_iterator<node, Const>::operator++() noexcept
@@ -111,9 +137,7 @@ tree_iterator<node, Const> tree_iterator<node, Const>::operator++() noexcept
         }
         next.current = (next->read_pr().get() != nullptr)
                          ? next->read_pr().get()
-                         :++current;
-                         //: &bst<int, int>::end_node;
-        //std::cout << "EGGS " << &Spam::eggs << '\n';
+                         :nullptr;
     }
 
     // Allow (N)RVO
@@ -122,9 +146,10 @@ tree_iterator<node, Const> tree_iterator<node, Const>::operator++() noexcept
 }
 
 /**
- * @brief Type-templated forward tree_iterator
+ * @brief Post-increment of forward iterator
  * @tparam node Typename of the node
  * @tparam Const Bool to represent a const iterator (true) or an iterator (false)
+ * @return this before being incremented
  */
 template<typename node, bool Const>
 inline tree_iterator<node, Const> tree_iterator<node, Const>::operator++(int) noexcept  // Acceptable warning (clang-tidy)
@@ -135,10 +160,11 @@ inline tree_iterator<node, Const> tree_iterator<node, Const>::operator++(int) no
 }
 
 /**
- * @brief Type-templated forward tree_iterator
+ * @brief return the iterator associated to the leaf obtained going leftmost from given
  * @tparam node Typename of the node
  * @tparam Const Bool to represent a const iterator (true) or an iterator (false)
- * @param given
+ * @param given the starting node
+ * @return iterator to the leftmost leaf obtained from the given node
  */
 template<typename node, bool Const>
 tree_iterator<node, Const> tree_iterator<node, Const>::leftmost(tree_iterator<node, Const>& given) noexcept
@@ -152,9 +178,10 @@ tree_iterator<node, Const> tree_iterator<node, Const>::leftmost(tree_iterator<no
 }
 
 /**
- * @brief Type-templated forward tree_iterator
+ * @brief returns the iterator pointing to the next node
  * @tparam node Typename of the node
  * @tparam Const Bool to represent a const iterator (true) or an iterator (false)
+ * @return next iterator
  */
 template<typename node, bool Const>
 inline tree_iterator<node, Const> tree_iterator<node, Const>::next() noexcept
